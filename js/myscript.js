581d666be6451e486148dfba828062e5f05b2e12
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-22 01:55:25 -0400

    added coindeskapi, changed background, and created sort functions

---
+++ js/myscript.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    $("#sortall").click(function () {
+        console.log("this works");
+        
+            $("#car").show();
+            $("#news").show();
+            $("#time").show();
+            $("#survey").show();
+            $("#coin").show();
+            $("#fetch").show();
+        
+    });
+
+    $("#sortclass").click(function () {
+        console.log("this works");
+        
+            $("#car").hide();
+            $("#news").hide();
+            $("#time").hide();
+            $("#survey").hide();
+            $("#coin").hide();
+
+            $("#fetch").show();
+        
+    });
+
+    $("#sortper").click(function () {
+        console.log("this works");
+        
+            $("#car").show();
+            $("#news").show();
+            $("#time").show();
+            $("#survey").show();
+            $("#coin").show();
+
+            $("#fetch").hide();
+        
+    });
+});
